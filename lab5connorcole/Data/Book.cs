9f61bcadd18d0319f1747bbd617a328e323dc87e
Connor <144731670+johnfkinnedy@users.noreply.github.com>
2024-11-25 01:35:47 -0500

    Add files via upload

---
+++ lab5connorcole/Data/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace lab5connorcole.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
---
+++ lab5connorcole/Data/User.cs
@@ -0,0 +1,9 @@
+﻿namespace lab5connorcole.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
